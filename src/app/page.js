3aaad7bdfdc847afe420237142be257f2c73f72b
hri-gh <hrithikgh.edu@gmail.com>
2023-12-17 12:25:10 +0530

    First commit

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World</h1>
+      <h1 className='text-red-500'>Hello World</h1>
+      <h2>Hello</h2>
     </main>
   )
 }
